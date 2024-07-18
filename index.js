4ab4ca5190ea55190ab62c1633b8e383a2905098
victormuteithia <muteithiavictor@gmail.com>
2024-07-19 00:49:05 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+function findMatching(collection, name){
+    // console.log(collection, name)
+    return collection.filter(function(value){
+        return value.toLowerCase() === name.toLowerCase()
+    })
+}
+
+function fuzzyMatch(collection, name){
+    return collection.filter((value) => {
+        return value.startsWith(name)
+    })
+}
+
+function matchName(collection, name){
+    return collection.filter((value) => {
+        return value["name"] === name
+    })
+}
